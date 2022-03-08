05197f00ee647ef9cd74368346e6999e3a2b2c92
Aman-shukla <aman.shukla200115@gmail.com>
2022-03-08 23:13:12 +0530

    Up

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
+
+function App() {
+  const [questions, setQuestions] = useState();
+  const [name, setName] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
+
+  return (
+    <BrowserRouter>
+      <div className="app" >
+        <Header />
+        <Routes>
+          <Route path="/" exact
+            element={<Home
+              name={name}
+              setName={setName}
+              fetchQuestions={fetchQuestions}
+            />}
+          />
+
+
+
+          <Route path="/quiz"
+           element={ <Quiz
+            name={name}
+            questions={questions}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+          />}
+        />
+
+
+
+          <Route path="/result"
+           element={<Result name={name} score={score} />} 
+          />
+
+
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import { SocialIcon } from 'react-social-icons'
+
+
+const Footer = () => {
+    return (
+      <div style={{textAlign:"center", marginBottom:'10'}}>
+      Devloped With 🫀 and 🧠 by Aman Shukla <br />
+     <SocialIcon url='https://www.linkedin.com/in/aman-shukla-485661190/'/>
+     <SocialIcon url='https://github.com/Aman-shukla'/>
+     <SocialIcon url='https://twitter.com/AmanShu15092001'/>
+     
+
+    </div>
+    );
+  };
+  
+  export default Footer;
